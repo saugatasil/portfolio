853ea4b65174a7b15b0ced42f9f01c69e3c176a5
Saugata <saugatasil@gmail.com>
2020-09-13 19:21:37 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,21 @@
+<?php
+
+  $sgtcnt_name = $_POST["sgtcnt_name"];
+  $sgtcnt_email = $_POST["sgtcnt_email"];
+  $sgtcnt_subject = $_POST["sgtcnt_subject"];
+  $sgtcnt_message = $_POST["sgtcnt_message"];
+
+if($sgtcnt_name == "" && $sgtcnt_email == ""){
+
+  $to = 'saugatasil@gmail.com';
+  $subject = 'hello';
+  $headers = "From: saugatasil@gmail.com";
+  $message = 'Hello World';
+
+  if(mail($to, $subject, $message, $headers)){
+    echo "success";
+  }else{
+    echo "error";
+  }
+}
+?>
